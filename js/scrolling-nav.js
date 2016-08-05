8f237411dd3d5b768fded17d8485e9b0097c97d5
Matthew Watson <schrodeingers.hat@gmail.com>
2016-08-06 07:41:56 +1000

    Extract data to JS

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -17,3 +17,54 @@ $(function() {
         event.preventDefault();
     });
 });
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.results').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 500, 'easeInOutExpo');
+        var house = {
+        live: $('[name="live"]:checked').val(),
+        block: $('[name="block"]:checked').val(),
+        bedrooms: $('#selector-bedrooms').val(),
+        carpark: $('#selector-carpark').val(),
+        size: $('#selector-size').val(),
+        age: $('#selector-age').val()
+        };
+        var postcode = parseInt($('.placepicker').val().match(/\d\d\d\d/));
+        postcode = isNaN(postcode) ? 4000 : postcode;
+        var buyer = {
+            adults: $('[name="adults"]:checked').val(),
+            debts: $('[name=debt]')
+                .map(function(i,el){return el.value})
+                .filter(function(i,el){n = parseFloat(el);return (!isNaN(n)) && n > 0;})
+                .map(function(i, el){return parseFloat(el)})
+                .toArray(),
+            savings: parseFloat($('#inputSavings').val()),
+            savingsInterval: $('#selectSavings').val(),
+            living: $('[name="living"]:checked').val(),
+            rent: parseFloat($('#inputRent').val()),
+            rentInterval: $('#selectRent').val(),
+        };
+        var finance = {
+            deposit: parseInt($('#inputDeposit').val()),
+            help: $('[name="help"]:checked').val(),
+            //Quick and dirty child age validation.
+            children: $('.child-age')
+                .map(function(i,el){return el.value})
+                .filter(function(i,el){n = parseInt(el);return (!isNaN(n)) && n > 0 && n < 21;})
+                .map(function(i, el){return parseInt(el)})
+                .toArray()
+        }
+
+
+
+        console.log(house);
+        console.log(postcode);
+        console.log(finance);
+        console.log(buyer);
+    });
+    event.preventDefault();
+});
